3239b4c258d680862272c71d3de15341b070e8b1
diamant1991 <diamant1988@mail.ru>
2017-03-29 15:34:20 +0300

    add files

--- js/script.js
+++ js/script.js
@@ -12,6 +12,44 @@ $(document).ready(function() {
       $('select.nice3').niceSelect();      
 });
 
+ var review_stars = $('.j-rating a');
+    $(review_stars).hover(function () {
+        $(this).parent().find(review_stars).removeClass('is-hover is-active m-select m-active');
+        $(this).addClass('is-hover');
+        var thisStar = $(this).parent().find(review_stars);
+        $(thisStar).each(function () {
+            $(this).addClass('is-active');
+            if ( $(this).hasClass('is-hover') ) {
+                return false;
+            }
+        });
+    }, function () {
+        var value = $(this).parent().find('input[type=hidden]').val();
+        $(this).parent().find(review_stars).removeClass('is-hover is-active m-select m-active');
+        var thisStar = $(this).parent().find(review_stars);
+        if( value >= 1 ) {
+            $(thisStar).each(function () {
+                $(this).addClass('m-select');
+                if ($(this).data('index') == value) {
+                    $(this).addClass('m-active');
+                    return false;
+                }
+            });
+        }
+    });
+    $(review_stars).click(function () {
+        $(this).addClass('m-active');
+        var thisStar = $(this).parent().find(review_stars);
+        $(thisStar).each(function () {
+            $(this).addClass('m-select');
+            if ( $(this).hasClass('m-active') ) {
+                return false;
+            }
+        });
+        $(this).parent().find('input[type=hidden]').val(parseInt($(this).data('index')));
+        return false;
+    });
+
 $(document).ready(function(){
   $('.view-item-carousel').owlCarousel({
   	loop:true,
@@ -55,11 +93,18 @@ $(document).ready(function(){
         }
     }
   });
-  $('#product-carousel').bxSlider({
-	  pagerCustom: '#bx-pager',
-	  controls: false,
-	  mode: 'fade'
-	});
+  if($('#product-carousel').length){
+  	$('#product-carousel').bxSlider({
+		  pagerCustom: '#bx-pager',
+		  controls: false,
+		  mode: 'fade'
+		});
+  }
+  $('.close-btn').click(function(e) {
+  	e.preventDefault();
+  	$(this).parent().remove()
+  });
+  
 });
 
 var tab = new Array('.tab-descr','.tab-char', '.tab-review', '.tab-gar', '.tab-del');
